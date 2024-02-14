ae22cd4274cd1ffc1267963c7ee5085b171d6b0f
EphriamHab <ephremhabtamu2015@gmail.com>
2024-02-14 20:20:11 +0300

    collection of question data

---
+++ Day 17/data.py
@@ -0,0 +1,84 @@
+
+question_data = [
+ 
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "General Knowledge",
+                "question": "The word \"news\" originates from the first letters of the 4 main directions on a compass (North, East, West, South).",
+                "correct_answer": "False",
+                "incorrect_answers": ["True"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "General Knowledge",
+                "question": "Slovakia is a member of European Union-",
+                "correct_answer": "True",
+                "incorrect_answers": ["False"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "General Knowledge",
+                "question": "Haggis is traditionally ate on Burns Night.",
+                "correct_answer": "True",
+                "incorrect_answers": ["False"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "General Knowledge",
+                "question": "The French word to travel is \"Travail\".",
+                "correct_answer": "False",
+                "incorrect_answers": ["True"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "General Knowledge",
+                "question": "Kissing someone for one minute burns about 2 calories.",
+                "correct_answer": "True",
+                "incorrect_answers": ["False"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "General Knowledge",
+                "question": "Jingle Bells was originally meant for Thanksgiving",
+                "correct_answer": "True",
+                "incorrect_answers": ["False"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "General Knowledge",
+                "question": "The commercial UK channel ITV stands for \"International Television\".",
+                "correct_answer": "False",
+                "incorrect_answers": ["True"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "General Knowledge",
+                "question": "Furby was released in 1998.",
+                "correct_answer": "True",
+                "incorrect_answers": ["False"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "General Knowledge",
+                "question": "The scientific name for the Southern Lights is Aurora Australis?",
+                "correct_answer": "True",
+                "incorrect_answers": ["False"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "General Knowledge",
+                "question": "Dihydrogen Monoxide was banned due to health risks after being discovered in 1983 inside swimming pools and drinking water.",
+                "correct_answer": "False",
+                "incorrect_answers": ["True"]
+            }
+]
