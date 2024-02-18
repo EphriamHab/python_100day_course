8fab95834d6830941545b28d6c61a0cf14627897
EphriamHab <ephremhabtamu2015@gmail.com>
2024-02-18 15:28:28 +0300

    color list added

--- Snake_Game_Day 20&21/food.py
+++ Snake_Game_Day 20&21/food.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 import random
+color_list = ["blue", "yellow","red","orange"]
 class Food(Turtle):
     def __init__(self):
         super().__init__()
@@ -12,5 +13,7 @@ class Food(Turtle):
     def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
+        random_color = random.choice(color_list)
+        self.color(random_color)
         self.goto(random_x, random_y)  
         self.showturtle()
